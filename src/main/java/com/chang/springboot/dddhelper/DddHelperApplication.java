09b1344eec3b484e250677bee391a49893e23f02
bbtree <dev@bbtree.co.kr>
2018-06-02 02:06:34 +0900

    init

---
+++ src/main/java/com/chang/springboot/dddhelper/DddHelperApplication.java
@@ -0,0 +1,12 @@
+package com.chang.springboot.dddhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddHelperApplication.class, args);
+    }
+}
---
+++ src/test/java/com/chang/springboot/dddhelper/DddHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chang.springboot.dddhelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DddHelperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
